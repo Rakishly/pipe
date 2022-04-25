3c07acca117a12a93f9ee46a1bea6e652ea11874
wuyue <wuyue@singbada.cn>
2022-04-25 10:58:19 +0800

    first commit

---
+++ src/contract/Pipeline.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace Wuyue\Pipe\contract;
+
+
+use Closure;
+
+interface Pipeline
+{
+    /**
+     * Set the traveler object being sent on the pipeline.
+     *
+     * @param  mixed  $traveler
+     * @return $this
+     */
+    public function send($traveler);
+
+    /**
+     * Set the stops of the pipeline.
+     *
+     * @param  array  $stops
+     * @return $this
+     */
+    public function through($stops);
+
+    /**
+     * Set the method to call on the stops.
+     *
+     * @param  string  $method
+     * @return $this
+     */
+    public function via($method);
+
+    /**
+     * Run the pipeline with a final destination callback.
+     *
+     * @param  \Closure  $destination
+     * @return mixed
+     */
+    public function then(Closure $destination);
+}
